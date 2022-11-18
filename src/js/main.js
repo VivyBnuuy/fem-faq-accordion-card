8b213d48fbddeb6cdf1659a6378f3cf74c48144b
LazyDuckling <emi@lazyduckling.dev>
2022-11-18 08:00:14 -0600

    Added the images to the background layout and changed the breakpoints from 920 to 980 px on the width for switching from mobile to desktop layouts

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 "use strict";
 
-// TODO: Edit chevron alt text so that it reads expand/collapse depending on state
+/* TODO:    1. Edit chevron alt text so that it reads expand/collapse depending on state
+            2. Flip chevron upside down when a question is shown
+            3. Toggle the display for the questions by adding an event listener on the questions
+*/
+
